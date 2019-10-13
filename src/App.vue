e8d76adfc3780f2efb4a862cb1afb72dc8f85ad1
linguangxiong <854501715@qq.com>
2019-10-14 00:54:35 +0800

    watch array and obj

--- src/App.vue
+++ src/App.vue
@@ -2,50 +2,81 @@
 
   <div id="app">
 
-    <div>
-      <span>Input num：</span>
-      <input type="text" v-model="counterNumber">
-    </div>
-
-    <CounterGroup></CounterGroup>
+    <Children @changeNum="getValue" :passFromFather="caculateNum"></Children>
+    <p>obj.a: {{obj.a}}</p>
+    <p>obj.a: <input type="text" v-model="obj.a"></p>
+    <input type="button" @click="this.testClick">
+    <!--<input v-model="array">-->
+    <p>array[0]: {{array[0]}}</p>
+    <p>array[0]: <input type="text" v-model="array[0]"></p>
+    <input type="button" @click="this.testClick2">
+    <p>{{value}}</p>
+    <p>{{caculateNum}}</p>
 
   </div>
 
 </template>
 
 <script>
-    import CounterGroup from './components/CounterGroup.vue'
+
+    import Children from './components/Children.vue'
 
     export default {
 
         name: 'app',
-        data() {
-            return {
-                counterNumber: 0
-            }
+
+        data(){
+          return{
+            value:0,
+            obj:{
+              a: 123
+            },
+            array:[1,2]
+          }
         },
+        methods:{
+
+          getValue(val){
+            this.value = val
+          },
+          testClick(){
+            this.obj.a = 3
+          },
+          testClick2(){
+            console.log('-----');
+            this.array[0] = 100;
+            // this.array.push(1);
+            console.log('-----1----');
+          }
+
+        },
+        computed:{
+
+          caculateNum() {
+            return this.value + 'computed'
+          }
+
+        },
+      watch:{
+        obj: {
+          handler(newName, oldName) {
+            console.log('obj.a changed');
+          },
+          deep: true
+        },
+        array:{
+          handler(val) {
+            console.log('val',val)
+          },
+          deep: true
+        }
+      },
 
         components: {
-            CounterGroup
+          Children
         },
 
-        watch: {
-            counterNumber: function () {
-                this.$store.commit('setCounterNum',this.counterNumber);
-            }
-        }
     }
 
 </script>
 
-
-<style>
-
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 20px;
-  }
-
-</style>
---
+++ src/components/Children.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <div>
+
+        <input v-model="test"/>
+
+        <p>{{passFromFather}}FromFather</p>
+
+    </div>
+
+
+</template>
+
+<script>
+    export default {
+        name:"children",
+        props:['passFromFather'],
+
+        data(){
+            return{
+                test:0
+            }
+        },
+
+        watch:{
+            test() {
+                console.log(this.test)
+                this.$emit('changeNum',this.test)
+            }
+        }
+
+    }
+
+</script>
